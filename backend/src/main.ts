950c9b8d3c2c9f40d96deceab045605374db0468
Tomas <tomascurto1@gmail.com>
2025-06-04 00:12:40 -0300

    correcciones pre deploy del back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
+// backend/src/main.ts
+import { Handler } from 'express';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, adapter);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.init();
 }
 bootstrap();
+
+export const handler: Handler = expressApp;
