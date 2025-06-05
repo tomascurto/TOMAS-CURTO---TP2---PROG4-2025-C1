0e1ac5653ea8c8ecd2dbf1d581b6fab75438e878
Tomas <tomascurto1@gmail.com>
2025-06-05 20:49:26 -0300

    re-correccion

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,35 +1,12 @@
-// backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { INestApplication } from '@nestjs/common';
-import express, { Request, Response } from 'express';
 import { AppModule } from './app.module';
-
-const server = express();
-
-server.get('/favicon.ico', (_req, res) => {
-  res.status(204).end();
-});
-let nestApp: INestApplication;
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors();
-  await app.init();
-  nestApp = app;
-  app.enableCors({
-  origin: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization,X-Requested-With,Accept-Language",
-  optionsSuccessStatus: 204,
-  credentials: true,
-});
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-export default async function handler(req: Request, res: Response) {
-  if (!nestApp) {
-    await bootstrap();
-  }
-  server(req, res); 
-}
