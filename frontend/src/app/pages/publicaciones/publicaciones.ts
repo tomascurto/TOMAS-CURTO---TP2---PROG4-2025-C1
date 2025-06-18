fc845883929b2330244a70cfdfe6a28e9f098275
Tomas <tomascurto1@gmail.com>
2025-06-18 00:21:52 -0300

    publicaciones funciona a medias

--- frontend/src/app/pages/publicaciones/publicaciones.ts
+++ frontend/src/app/pages/publicaciones/publicaciones.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { PublicacionesService } from '../../services/publicaciones.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-publicaciones',
-  imports: [FormsModule],
-  templateUrl: './publicaciones.css', 
+  imports: [FormsModule, CommonModule],
+  templateUrl: './publicaciones.html',
+  styleUrls: ['./publicaciones.css']
 })
 export class Publicaciones {
   publicaciones: any[] = [];
@@ -18,9 +20,26 @@ export class Publicaciones {
   }
 
   cargarPublicaciones() {
-    this.publicacionesService.listar(this.orden, this.offset, this.limit).subscribe(data => {
-      this.publicaciones = data;
-    });
+  this.publicacionesService.listar('fecha', 0, 10).subscribe((data) => {
+    this.publicaciones = data.map((pub: any) => ({
+      ...pub,
+      yaLeGusta: pub.likes.includes(this.usuarioId),
+    }));
+  });
+}
+usuarioId = 'usuario-demo'; 
+  toggleLike(pub: any) {
+    const yaLeGusta = pub.likes?.includes(this.usuarioId);
+
+    if (yaLeGusta) {
+      this.publicacionesService.unlike(pub._id).subscribe(() => {
+        pub.likes = pub.likes.filter((id: string) => id !== this.usuarioId);
+      });
+    } else {
+      this.publicacionesService.like(pub._id).subscribe(() => {
+        pub.likes.push(this.usuarioId);
+      });
+    }
   }
 
   paginaAnterior() {
